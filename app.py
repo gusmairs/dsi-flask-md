0562ef7f8b3834405eb8b6a6edf48378e9cb8ed0
Gus <gusmairs@mac.com>
2019-05-20 14:20:47 -0700

    three inputs posting correctly

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/solve', methods=['POST'])
+def solve_it():
+    params = request.json
+    a = params['a_value']
+    b = params['b_value']
+    c = params['c_value']
+    return jsonify({'values': str(a) + str(b) + str(c)})

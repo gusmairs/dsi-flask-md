a9f6094401e759928034078673d450572dc0619e
Gus <gusmairs@mac.com>
2019-05-20 14:52:00 -0700

    solves for real

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, jsonify
+import numpy as np
 
 app = Flask(__name__, static_url_path='')
 
@@ -9,7 +10,8 @@ def index():
 @app.route('/solve', methods=['POST'])
 def solve_it():
     params = request.json
-    a = params['a_value']
-    b = params['b_value']
-    c = params['c_value']
-    return jsonify({'values': str(a) + str(b) + str(c)})
+    a = int(params['a_value'])
+    b = int(params['b_value'])
+    c = int(params['c_value'])
+    ans = [(np.sqrt(b**2 - 4 * a * c) + s * b) / (2 * a) for s in [1, -1]]
+    return jsonify({'values': ans})

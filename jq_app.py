ceb40caf7297d50ce3cade33c77b8fee18b1de85
Gus <gusmairs@mac.com>
2019-05-23 13:29:30 -0700

    added imaginary roots and some styling

--- jq_app.py
+++ jq_app.py
@@ -12,6 +12,10 @@ def solve_it():
     a, b, c = (request.json['a'],
                request.json['b'],
                request.json['c'])
-    disc = np.sqrt(b**2 - 4 * a * c) * np.array([1, -1])
-    roots = np.round((disc - b) / (2 * a), 3)
+    disc = b**2 - 4 * a * c
+    if disc >= 0:
+        roots = [np.round((np.sqrt(disc) - b) / (2 * a), 3),
+                 np.round((-np.sqrt(disc) - b) / (2 * a), 3)]
+    else:
+        roots = ['(imag)', '(imag)']
     return jsonify({'root_1': roots[0], 'root_2': roots[1]})
